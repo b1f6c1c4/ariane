cadd2d70200fa4d131ebbab496b7df727f02294a
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-06-02 06:35:47 -0400

    minor adjust

--- ci/build-riscv-gcc.sh
+++ ci/build-riscv-gcc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 mkdir -p ~/x-tools/
-[ ! -d ~/x-tools/riscv64-unknown-elf ] && (curl -fsSL http://b1f6c1c4.info/rv64.tar | tar -C ~/x-tools/ -xv)
+[ -d ~/x-tools/riscv64-unknown-elf ] || (curl -fsSL http://b1f6c1c4.info/rv64.tar | tar -C ~/x-tools/ -xv)
 ln -s ~/x-tools/riscv64-unknown-elf tmp/ricsv-gnu-toolchain
--- ci/get-sv2v.sh
+++ ci/get-sv2v.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-[ ! -x ~/sv2v/bin/sv2v ] && git get -g -o ~/sv2v --rm-rf https://github.com/zachjs/sv2v
+set -euxo pipefail
+
+[ -x ~/sv2v/bin/sv2v ] || git get -v -g -o ~/sv2v --rm-rf https://github.com/zachjs/sv2v
