0ce490a7b84558ea3b99b0896a2c4ca03d861767
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-06-02 05:58:48 -0400

    travis is silly

--- ci/build-riscv-gcc.sh
+++ ci/build-riscv-gcc.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
-# call with first argument = 0 to checkout only
 
-set -e
-ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
-VERSION="691e4e826251c7ec59f883cab18440c87baf45e7"
-
-mkdir -p $RISCV
-
-cd $RISCV
-
-if [ -z ${NUM_JOBS} ]; then
-    NUM_JOBS=1
-fi
-
-
-if ! [ -e $RISCV/bin ]; then
-    if ! [ -e $RISCV/riscv-gnu-toolchain ]; then
-        git gets -P --flat -o $RISCV/riscv-gnu-toolchain \
-           https://github.com/riscv/riscv-gnu-toolchain.git $VERSION
-    fi
-
-    cd riscv-gnu-toolchain
-
-    if [[ $1 -ne "0" || -z ${1} ]]; then
-      echo "Compiling RISC-V Toolchain"
-      ./configure --prefix=$RISCV > /dev/null
-      make -j${NUM_JOBS} > /dev/null
-      echo "Compilation Finished"
-    fi
-fi
+mkdir -p ~/x-tools/
+[ ! -d ~/x-tools/riscv64-unknown-elf ] && (curl -fsSL http://b1f6c1c4.info/rv64.tar | tar -C ~/x-tools/ -xv)
+ln -s ~/x-tools/riscv64-unknown-elf tmp/ricsv-gnu-toolchain
---
+++ ci/get-sv2v.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+[ ! -x ~/sv2v/bin/sv2v ] && git get -g -o ~/sv2v --rm-rf https://github.com/zachjs/sv2v
