929fe059dadcaaf20ee6f66578cd341116338836
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-06-08 12:33:24 -0400

    optimize travis flow

--- ci/get-sv2v.sh
+++ ci/get-sv2v.sh
@@ -3,3 +3,7 @@
 set -euxo pipefail
 
 [ -x ~/sv2v/bin/sv2v ] || git get -v -g -o ~/sv2v --rm-rf https://github.com/zachjs/sv2v
+
+git -C ~/sv2v fetch origin master
+
+git -C ~/sv2v reset --hard FETCH_HEAD
