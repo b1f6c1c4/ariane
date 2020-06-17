d1cc371c519c82bf41224acd7891d54deaca3dfe
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-06-16 20:57:52 -0400

    fix silly bug

--- ci/get-sv2v.sh
+++ ci/get-sv2v.sh
@@ -2,11 +2,14 @@
 
 set -euxo pipefail
 
-[ -x ~/sv2v/bin/sv2v ] || git get -v -g -o ~/sv2v --rm-rf https://github.com/zachjs/sv2v
+if [ ! -x ~/sv2v/bin/sv2v ]; then
+    git get -v -g -o ~/sv2v --rm-rf https://github.com/zachjs/sv2v
+    make -C ~/sv2v
+fi
 
 git -C ~/sv2v fetch origin master:FETCH_HEAD
 
-if [ "$(git rev-parse HEAD)" = "$(git rev-parse FETCH_HEAD)" ]; then
+if [ ! "$(git -C ~/sv2v rev-parse HEAD)" = "$(git -C ~/sv2v rev-parse FETCH_HEAD)" ]; then
     git -C ~/sv2v reset --hard FETCH_HEAD
     make -C ~/sv2v
 fi
